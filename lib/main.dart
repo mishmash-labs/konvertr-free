725836e39b552b5ab03d1b6b5529886620a211c5
Fawad <runedrune@gmail.com>
2020-07-12 19:29:39 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/category_provider.dart';
@@ -8,18 +10,28 @@ import 'screens/categories_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(KonvertrHome());
+  final delegate = await LocalizationDelegate.create(
+      fallbackLocale: 'en', supportedLocales: ['en', 'es', 'fr', 'de', 'hi']);
+  runApp(LocalizedApp(delegate, KonvertrHome()));
 }
 
 class KonvertrHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final localizationDelegate = LocalizedApp.of(context).delegate;
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
             create: (context) => CategoriesProvider(context), lazy: false),
       ],
       child: MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          localizationDelegate
+        ],
+        supportedLocales: localizationDelegate.supportedLocales,
+        locale: localizationDelegate.currentLocale,
         home: CategoriesScreen(),
         theme: ThemeData(
           primaryColor: Color(0xff380e7f),
